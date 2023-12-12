e2abc69b5afb152fdec8d9a17168a8804f36bd6b
Thet-Naing799555 <lester6586@gmail.com>
2023-12-12 21:15:15 +0630

    tailin config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,39 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+
+
+    extend: {
+
+
+      fontFamily: {
+        sans: ['Rethink Sans', 'sans-serif'],
+        serif: ['Silkscreen', 'serif'],
+        mono: ['Silkscreen', "monospace"],
+      },
+
+
+      colors: {
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+    
+
+    },
+
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+
+
+  ],
 }
 
